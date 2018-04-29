847b3fd146b1bb7b187459c646536e71e998aa4a
wonwoo <aoruqjfu@gmail.com>
2018-04-29 22:19:52 +0900

    save delete

--- src/main/java/ml/wonwoo/springdatacommonexample/read/ReadRepository.java
+++ src/main/java/ml/wonwoo/springdatacommonexample/read/ReadRepository.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 @NoRepositoryBean
 public interface ReadRepository<T, ID> extends Repository<T, ID>{
 
-  <S extends T> Iterable<S> saveAll(Iterable<S> var1);
-
   Optional<T> findById(ID var1);
 
   boolean existsById(ID var1);
